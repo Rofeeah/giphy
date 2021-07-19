a22984cc63b5ce21da196f2bd329679e7115ad51
Rofeeah <ayenirofeeah@gmail.com>
2021-07-19 18:12:01 +0000

    Add git code that is not complete yet

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask, render_template, request
 from datetime import datetime
-# from model import getImageUrlFrom
+from model import getImageUrlFrom
 import os
 
 # -- Initialization section --
@@ -14,3 +14,19 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template("index.html", time = datetime.now())
+
+# add route for giph results
+
+@app.route("/yourgif",methods = ['GET', 'POST'] )
+def yourgif():
+    # gets the giph for giphy and puts the link on webpage
+    
+    user_response = request.form['gifchoice']
+    
+    giph_link = getImageUrlFrom(user_response)
+    print(giph_link)
+    # pass url to render template and have that url appear as an image in yourgif.html
+    return render_template("yourgif.html",time = datetime.now(), giph_link = giph_link)
+    # datetime.now gets the updated time and code for css. Tricks browser in updating to think its real time.
+
+# print(request.form['gifchoice'])
--- model.py
+++ model.py
@@ -1,2 +1,8 @@
+import requests
+
+
 def getImageUrlFrom(query):
-    pass
+    giphy_request_link = "https://api.giphy.com/v1/gifs/search?api_key=v4W0oPRrBCUAs0lSSukNDVWUPRYwh1do&q=+cat+&limit=25&offset=0&rating=g&lang=en"
+    response = requests.get(giphy_request_link).json()
+    return response['data'][0]['images']['fixed_height']['url']
+
